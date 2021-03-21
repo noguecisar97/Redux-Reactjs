d94b1a8e6b7fad82414db723e2df95a69e3f6a0f
Júlio Cesar <dev26.osasbh@outlook.com>
2021-03-21 10:26:20 -0300

    adicionado styled-components no projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import './App.css';
 import SearchBar from './components/searchBar';
 import VideoList from './components/videoList';
 import VideoPlayer from './components/videoPlayer';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <SearchBar />
       <VideoList />
       <VideoPlayer />
